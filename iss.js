ec182d11a52d507d4321be3a9d42ad776d85dfd6
GSingh1994 <singh.gagandeep0034@gmail.com>
2022-04-14 16:11:54 -0500

    Add function to get coodinates using ip address

--- iss.js
+++ iss.js
@@ -20,4 +20,25 @@ const fetchMyIP = (callback) => {
   });
 };
 
-module.exports = { fetchMyIP };
+const key = '63646cd0-bc33-11ec-9451-c3c5a65bf186';
+
+const fetchCoordsByIP = (ip, callback) => {
+  request(`https://api.freegeoip.app/json/${ip}?apikey=${key}`, (error, response, body) => {
+    if (error) {
+      callback(error, null);
+      return;
+    }
+
+    if (response.statusCode !== 200) {
+      const msg = `Status Code ${response.statusCode} when fetching coordinates for IP. Response: ${body}`;
+      callback(Error(msg), null);
+      return;
+    }
+
+    const { latitude, longitude } = JSON.parse(body);
+
+    callback(error, { latitude, longitude });
+  });
+};
+
+module.exports = { fetchMyIP, fetchCoordsByIP };
